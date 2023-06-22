2727fa33714db8a730f3842f46023d59dcd99791
Shinyu Nikko <shinyunikko@gmail.com>
2023-06-22 08:36:21 +0000

    Build a NavBar

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
 
